9218ec41d00eef28d2fec37b92213383eeaf49ef
GitAvi001 <avindusridharmawardhana@gmail.com>
2026-04-28 11:45:19 +0530

    Authentication and Subscription configured with clerk

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { PricingTable } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <PricingTable/>
+    </div>
+  );
+}
